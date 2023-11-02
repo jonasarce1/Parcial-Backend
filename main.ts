f3f8414215bcf8aefc821fca5d5cac1d2e23a1af
jonasarce1 <jonasarceg@gmail.com>
2023-11-02 19:15:36 +0100

    archivos para la inicializacion de deno deploy

---
+++ main.ts
@@ -0,0 +1,22 @@
+import express from "npm:express@4.18.2";
+import mongoose from "npm:mongoose@7.6.3";
+
+import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
+const env = await load();
+
+const MONGO_URL = env.MONGO_URL || Deno.env.get("MONGO_URL"); //Obtenemos la variable de entorno MONGO_URL ya sea de .env o de las variables de entorno del sistema
+
+if(!MONGO_URL){
+  console.log("No se ha encontrado la variable de entorno MONGO_URL");
+  Deno.exit(1);
+}
+
+await mongoose.connect(MONGO_URL);
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.listen(3000, () => console.log("Servidor activo en puerto 3000"));
